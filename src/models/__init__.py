a334245b7f79c9feddda62e038c54663a24a3d4c
junting <panjunting@G105E1900267.local>
2019-06-14 23:39:42 +0800

    migrating source code to https://github.com/STVIR/seg2vid

--- src/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .multiframe_genmask import *
-from .multiframe_w_mask_genmask import *
-from .multiframe_w_mask_genmask_two_path import *
-from .multiframe_w_mask_genmask_two_path_iterative import *
-from .vgg_utils import *
--- src/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import *
-from .ops import *
-
